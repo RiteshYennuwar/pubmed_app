ca1aab20e8587014ebeb9403bab818d7973e8c88
Ritesh Yennuwar <yennuwarritesh@hotmail.com>
2026-01-18 15:00:48 -0500

    Update .env.example and settings.py to include database configuration variables

--- src/pubmed_app/config/settings.py
+++ src/pubmed_app/config/settings.py
@@ -4,6 +4,11 @@ from functools import lru_cache
 class Settings(BaseSettings):
     PUBMED_EMAIL: str
     PUBMED_API_KEY: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
 
     class Config:
         env_file = ".env"
